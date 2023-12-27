15b47d5ded2fd57d491db54b020a930cea734572
TiagoSansao <tiagossansao@gmail.com>
2023-12-27 01:00:28 -0300

    feat(docs): add swagger at /docs

--- email-register-api/src/api/server.ts
+++ email-register-api/src/api/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerData from '../../docs/swagger.json';
 import { routes } from './routes';
 import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware';
 
 const api = express();
 
+api.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerData));
 api.use('/api/', routes);
 api.use(errorHandlerMiddleware);
 
