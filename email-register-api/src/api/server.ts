dc9b63e5fbfad3260623fa397430919c8d2cdd8e
TiagoSansao <tiagossansao@gmail.com>
2023-12-27 02:12:53 -0300

    feat(email-register-api): add front-end being served

--- email-register-api/src/api/server.ts
+++ email-register-api/src/api/server.ts
@@ -6,8 +6,10 @@ import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware';
 
 const api = express();
 
-api.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerData));
+api.use(express.static('public'));
+
 api.use('/api/', routes);
+api.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerData));
 api.use(errorHandlerMiddleware);
 
 export { api };
